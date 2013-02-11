7aa612c9462b917976ce205c3565868940b7c0bd
Gustavo Felisberto <humpback@felisberto.net>
2013-02-11 00:12:29 +0000

    We have a visible map centered on the location of the report

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,9 +1,8 @@
 function initialize_map(elementId, latitude, longitude) {
-    alert( latitude +" "+ longitude)
     var mapOptions = {
         center: new google.maps.LatLng(latitude, longitude),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById(elementId),
         mapOptions);
