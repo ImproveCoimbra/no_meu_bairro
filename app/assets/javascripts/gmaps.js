181d305af3ec9b3359fcdbdad4597afae25c78f7
Gustavo Felisberto <humpback@felisberto.net>
2013-02-11 00:24:49 +0000

    And now a small marker.

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,9 +1,23 @@
 function initialize_map(elementId, latitude, longitude) {
+
+    var location = new google.maps.LatLng(latitude, longitude);
+
     var mapOptions = {
-        center: new google.maps.LatLng(latitude, longitude),
+        center: location,
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById(elementId),
         mapOptions);
+
+
+    var markerOptions = {
+        map: map,
+        position: location,
+        raiseOnDrag: false,
+        visible: true
+    }
+
+    var marker = new google.maps.Marker(markerOptions);
+
 }
