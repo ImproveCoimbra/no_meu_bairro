cc81f9bf967a30457d454cb6a5bbb9fc326df171
Gustavo Felisberto <humpback@felisberto.net>
2013-02-16 17:59:29 +0000

    Validation of coordinates in correct format and number

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class Report
   include Mongoid::Document
   include Mongoid::Paranoia
@@ -11,7 +13,7 @@ class Report
   field :closure_date, type: DateTime
 
 
-  index({ coordinates: "2d" })
+  index({coordinates: "2d"})
 
   ##I have to belong to a user
   belongs_to :user
@@ -20,13 +22,26 @@ class Report
 
   validates :description, presence: true
   validates :coordinates, presence: true
+  validate :validate_coordinates_format
 
   # Callbacks
   before_create :find_municipality, :convert_location
   after_create :bitch
 
   def convert_location
-    self.coordinates.map!{|c| c.to_f}
+    self.coordinates.map! { |c| c.to_f }
+  end
+
+  def validate_coordinates_format
+    if self.coordinates.size != 2
+      errors.add(:coordinates, "tem tamanho inválido")
+    end
+
+    begin
+      self.coordinates.each { |x| Float(x) }
+    rescue
+      errors.add(:coordinates, "tem um formato inválido")
+    end
   end
 
   def find_municipality
@@ -45,7 +60,7 @@ class Report
 
     first_loc = [80.24958300000003, 13.060422]
     second_loc = [81.24958300000003, 12.060422]
-    reports = Report.where(:coordinates => { "$within" => {"$box" => [first_loc, second_loc]} })
+    reports = Report.where(:coordinates => {"$within" => {"$box" => [first_loc, second_loc]}})
     reports[0].coordinates
 
   end
