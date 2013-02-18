6cdc2b1fc224061aeff66b120b95255ca28082cc
Gustavo Felisberto <humpback@felisberto.net>
2013-02-18 21:40:09 +0000

    Hide file selector for unsupported browsers.

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,5 +1,17 @@
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
+  var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
 }
+
+
+$(document).ready(function () {
+    var photosDiv = $('div#photos-div');
+    if (photosDiv.length > 0) {
+        if (!isFileInputSupported) {
+            photosDiv.hide();
+            $('div#no-photos-div').show();
+        }
+    }
+
+});
