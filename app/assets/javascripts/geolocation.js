ef448fb8a7b92a05f8fe796e5c49a15967426ff2
Gustavo Felisberto <humpback@felisberto.net>
2013-03-03 21:41:18 +0000

    Added ajax-loader gif to the loading of location from gps some i18n of location messages animated messages

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,24 +1,41 @@
 $(document).ready(function () {
 
+    var loading_location_message = $("#loading_location_message");
+    loading_location_message.hide();
+    loading_location_message.css("visibility", "visible");
+    var location_found_message = $("#location_found_message");
+    location_found_message.hide();
+    location_found_message.css("visibility", "visible");
+    var location_not_found_message = $("#location_not_found_message");
+    location_not_found_message.hide();
+    location_not_found_message.css("visibility", "visible");
+
+
     function findLocation() {
+        loading_location_message.show(500);
+        location_not_found_message.hide(500);
+
         navigator.geolocation.getCurrentPosition(
             foundLocation,
             noLocation,
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 0 }
+            { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 }
         );
     }
 
     function foundLocation(position) {
+        loading_location_message.hide(500);
+        location_found_message.show(500);
+
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
         document.getElementById('latitude').value = latitude;
         document.getElementById('longitude').value = longitude;
-        //TODO Localization
-        document.getElementById('status_message').innerHTML = 'Nota: A sua localização foi recolhida automaticamente.'
+
     }
-    //TODO Localization
+
     function noLocation() {
-        document.getElementById('status_message').innerHTML = 'Não foi possível validar a localização.<br/> Verifique as definições do seu browser.'
+        loading_location_message.hide(500);
+        location_not_found_message.show(500);
     }
 
     if ($('form#new_report').length > 0) {
