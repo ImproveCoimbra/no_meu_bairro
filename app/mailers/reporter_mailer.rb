66c9727680498f5517ffe2404e2ee4adf79a89b7
Gustavo Felisberto <humpback@felisberto.net>
2013-03-05 12:17:20 +0000

    Send reporter mail to the correct destination Do not present the edit link by default

--- app/mailers/reporter_mailer.rb
+++ app/mailers/reporter_mailer.rb
@@ -4,8 +4,7 @@ class ReporterMailer < ActionMailer::Base
 
   def reporter_email(report)
     @report = report
-    @url = "http://bairro.herokuapp.com/report/#{report.id}"
-    name_and_mail_destination = "#{report.municipality.driver_parameters["destination_name"]} <#{report.municipality.driver_parameters["destination_mail"]}>"
+    name_and_mail_destination = "#{report.user.uuid}"
     mail(:to => name_and_mail_destination, :subject => "Editar Situação Reportada em #{report.municipality.name} - #{report.id}")
   end
 end
