334a86ddad52f3ba8cbb246c67b608f092141bcf
Gustavo Felisberto <humpback@felisberto.net>
2013-03-21 23:11:55 +0000

    Create large thumbnail. Modal show if report image.

--- app/models/report_photo.rb
+++ app/models/report_photo.rb
@@ -4,7 +4,11 @@ class ReportPhoto
 
   embedded_in :report, :inverse_of => :photos
 
-  has_mongoid_attached_file :attachment, :styles => { :thumb => "40x40#", :small => "100x100#", :medium => "400x400>" }
+  has_mongoid_attached_file :attachment, :styles => { :thumb => '40x40#',
+                                                      :small => '100x100#',
+                                                      :medium => '400x400>',
+                                                      :large => '1024x1024>'
+  }
   attr_accessible :attachment
 
   def styles
