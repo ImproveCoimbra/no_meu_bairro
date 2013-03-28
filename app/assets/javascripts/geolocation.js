4476efb055b4d8d6d5c3390b1005d9014895cbb2
Gustavo Felisberto <humpback@felisberto.net>
2013-03-28 15:22:03 +0000

    Green button on submit. Changed order of back and submit, do not show that map before we have a position.

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,12 +1,14 @@
 var loading_location_message;
 var location_found_message;
 var location_not_found_message;
+var map_container;
 
 
 if ($('form#new_report').length > 0) {
     loading_location_message = $("#loading_location_message");
     location_found_message = $("#location_found_message");
     location_not_found_message = $("#location_not_found_message");
+    map_container = $("#problem");
 }
 
 function findLocation() {
@@ -23,6 +25,7 @@ function findLocation() {
 function foundLocation(position) {
     loading_location_message.hide(500);
     location_found_message.show(500);
+    map_container.css("visibility","visible");
 
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
@@ -38,6 +41,7 @@ function foundLocation(position) {
 function noLocation() {
     loading_location_message.hide(500);
     location_not_found_message.show(500);
+    map_container.css("visibility","visible");
 
     var latitude = 40.1950631;
     var longitude = -8.419715912072775;
