c0c11cb7b854a8a896650c290a801e0e2f35867c
Gustavo Felisberto <humpback@felisberto.net>
2013-04-29 21:08:02 +0100

    Only have the styles we use. Improves performance.

--- app/models/report_photo.rb
+++ app/models/report_photo.rb
@@ -4,14 +4,14 @@ class ReportPhoto
 
   embedded_in :report, :inverse_of => :photos
 
-  has_mongoid_attached_file :attachment, :styles => { :thumb => '40x40#',
-                                                      :small => '100x100#',
-                                                      :medium => '400x400>',
-                                                      :large => '1024x1024>'
+  has_mongoid_attached_file :attachment, :styles => {#:thumb => '40x40#',
+                                                     #:small => '100x100#',
+                                                     :medium => '400x400>',
+                                                     :large => '1024x1024>'
   }
   attr_accessible :attachment
 
   def styles
-    Hash[ (self.attachment.styles.keys + [:original]).map{|style| [style, self.attachment.url(style)]} ]
+    Hash[(self.attachment.styles.keys + [:original]).map { |style| [style, self.attachment.url(style)] }]
   end
 end
