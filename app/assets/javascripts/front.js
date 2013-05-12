0f0e8b554d657132f63572cc5ecec86c24937e25
Gustavo Felisberto <humpback@felisberto.net>
2013-05-12 22:11:49 +0100

    Clustering - Work in progress
    * It should work acording to http://stackoverflow.com/questions/12055042/gmaps4rails-whats-the-proper-way-to-enable-clustering-after-adding-markers-via but nothing happens

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -85,7 +85,9 @@ jQuery(function ($) {
 
   Gmaps.map.callback = function() {
 
+    Gmaps.map.markers_conf.do_clustering = true;
     // Map fully loaded here
+
     google.maps.event.addListenerOnce(Gmaps.map.serviceObject, 'idle', updateMarkers);
 
     google.maps.event.addListener(Gmaps.map.serviceObject, 'bounds_changed', updateMarkers);
