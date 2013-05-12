08c8a8cf9589d83d7caa36d4a6d0845f97c4edaf
Gustavo Felisberto <humpback@felisberto.net>
2013-05-12 19:23:23 +0100

    Dynamic Markers
    * Removed loading of markers in FrontController
    * Changed front controller view to have map centered in Coimbra. This way the initial load of the map will request the markers from the ReportsController

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,10 +1,10 @@
 class FrontController < ApplicationController
 
   def index
-    @reports = Report.coimbra.to_gmaps4rails do |report, marker|
-      marker.title report.description
-      marker.json({:link => report_url(report)})
-    end
+    #@reports = Report.coimbra.to_gmaps4rails do |report, marker|
+    #  marker.title report.description
+    #  marker.json({:link => report_url(report)})
+    #end
   end
 
 
