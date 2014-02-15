52b51e5625b1b975a53049fbba6cf90ad1bdbb1e
Luis Cardoso <luis.cardoso@feedzai.com>
2014-02-15 17:55:50 +0000

    Still wip on the image downsizer

--- app/assets/javascripts/imageDownsizer.js
+++ app/assets/javascripts/imageDownsizer.js
@@ -1,13 +1,34 @@
+/**
+TODO:
+- Test it!
+- Find a way to not upload the actual inputs if the downsize works!
+*/
+
 var MAX_WIDTH = 200;
 var MAX_HEIGHT = 300;
 
-function addSmallerImagesToForm (done) {
+function isCanvasSupported(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+}
+
+//Reuse de same canvas
+if (isCanvasSupported()) {
+	var canvas = $("<canvas></canvas>")[0];
+	var ctx = canvas.getContext("2d");
+}
+
+function addSmallerImagesToForm () {
 	var $fileInputs, $newReport;
+
+	if (!window.FileReader || !isCanvasSupported() || !canvas.toDataURL) {
+		return $.Deferred().resolve();
+	}
 
 	$newReport = $("#new_report");
 	$fileInputs = $("#new_report input[type=file]");
 
-	$fileInputs.each(function (argument) {
+	return $.when.apply(null, $fileInputs.map(function (argument) {
 		var $fileInput, files;
 
 		$fileInput = $(this);
@@ -15,60 +36,73 @@ function addSmallerImagesToForm (done) {
 		files = $fileInput[0].files;
 
 		if (files.length > 0 && files[0].type.match(/image.*/)) {
-			addSmallerImage($fileInput, $newReport, files[0]);
+			return addSmallerImage($fileInput, $newReport, files[0]);
 		}
-	});
-}
 
-var canvas = $("<canvas></canvas>")[0];
-var ctx = canvas.getContext("2d");
+		return $.Deferred().resolve();
+	}));
+}
 
 //Based on https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
 function addSmallerImage ($fileInput, $form, file) {
-	var img = document.createElement("img");
-	var reader = new FileReader();  
+	var img, reader, defer;
+
+	img = document.createElement("img");
+	reader = new FileReader();
+	defer = $.Deferred();
+
+	reader.onabort = reader.onerror = function (argument) {
+		defer.reject();
+	};
 	
 	reader.onload = function(e) {
 		var index, dataurl, $hiddenInput;
 
-		img.src = e.target.result;
+		try {
+			img.src = e.target.result;
 		
-		//Do the resizing
-		var width = img.width;
-		var height = img.height;
-		if (width > height) {
-		  if (width > MAX_WIDTH) {
-		    height *= MAX_WIDTH / width;
-		    width = MAX_WIDTH;
-		  }
-		} else {
-		  if (height > MAX_HEIGHT) {
-		    width *= MAX_HEIGHT / height;
-		    height = MAX_HEIGHT;
-		  }
+			//Do the resizing
+			var width = img.width;
+			var height = img.height;
+			if (width > height) {
+			  if (width > MAX_WIDTH) {
+			    height *= MAX_WIDTH / width;
+			    width = MAX_WIDTH;
+			  }
+			} else {
+			  if (height > MAX_HEIGHT) {
+			    width *= MAX_HEIGHT / height;
+			    height = MAX_HEIGHT;
+			  }
+			}
+			canvas.width = width;
+			canvas.height = height;
+			ctx.drawImage(img, 0, 0, width, height);
+
+			//Convert the resize image in the cavas to base 64
+			dataurl = canvas.toDataURL("image/jpeg");
+			dataurl = dataurl.slice("data:image/jpeg;base64,".length);
+
+			//Get the timestamp out of the name, which is like 'report[photos_attributes][1392484594913][attachment]'
+			index = $fileInput.prop("name").split("[")[2].split("]")[0]
+
+			//Adds the hidden input that contains the image as base64
+			$hiddenInput = $('<input type="hidden" />');
+			$hiddenInput.prop({
+				value: dataurl,
+				name: "report[photos_attributes_base64][" + index + "]"
+			});
+			$form.append($hiddenInput);
+
+			//TODO: this may be necessary in FF
+			//var file = canvas.mozGetAsFile("foo.png");
+		} catch (e) {
+			defer.reject(e);
 		}
-		canvas.width = width;
-		canvas.height = height;
-		ctx.drawImage(img, 0, 0, width, height);
 
-		//Convert the resize image in the cavas to base 64
-		dataurl = canvas.toDataURL("image/jpeg");
-
-		//TODO: remove the leading stuff from the data url
-
-		//Get the timestamp out of the name, which is like 'report[photos_attributes][1392484594913][attachment]'
-		index = $fileInput.prop("name").split("[")[2].split("]")[0]
-
-		//Adds the hidden input that contains the image as base64
-		$hiddenInput = $('<input type="hidden" />');
-		$hiddenInput.prop({
-			value: dataurl,
-			name: "report[photos_attributes_base64][" + index + "]"
-		});
-		$form.append($hiddenInput);
-
-		//TODO: this may be necessary in FF
-		//var file = canvas.mozGetAsFile("foo.png");
-	}
+		defer.resolve();
+	};
 	reader.readAsDataURL(file);
+
+	return defer.promise();
 }
